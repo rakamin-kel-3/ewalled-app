845b54e1ffa784b985f2a0e45fa4f7f64063c587
kobulwidodo <fadhilhan01@gmail.com>
2025-03-23 16:51:52 +0700

    feat: init tabs

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,82 @@
+import { Tabs } from "expo-router";
+import { Image, ImageSourcePropType, Text, View } from "react-native";
+
+import icons from "@/constants/icons";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: ImageSourcePropType;
+  title: string;
+}) => (
+  <View className="flex-1 mt-3 flex flex-col items-center">
+    <Image
+      source={icon}
+      tintColor={focused ? "#0061FF" : "#666876"}
+      resizeMode="contain"
+      className="size-6"
+    />
+    <Text
+      className={`${
+        focused
+          ? "text-primary-300 font-rubik-medium"
+          : "text-black-200 font-rubik"
+      } text-xs w-full text-center mt-1`}
+    >
+      {title}
+    </Text>
+  </View>
+);
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#0061FF1A",
+          borderTopWidth: 1,
+          minHeight: 70,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="topup"
+        options={{
+          title: "Top Up",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.plus} title="Top Up" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="transfer"
+        options={{
+          title: "Transfer",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.send} title="Transfer" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
