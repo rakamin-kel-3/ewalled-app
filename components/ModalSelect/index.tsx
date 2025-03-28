967c3ded755841307b7b3e48e0b24a2b1ebf6fc9
kobulwidodo <fadhilhan01@gmail.com>
2025-03-28 16:43:11 +0700

    feat: implement typesafe

--- components/ModalSelect/index.tsx
+++ components/ModalSelect/index.tsx
@@ -1,3 +1,4 @@
+import { PaymentOptions } from "@/model/modal";
 import React from "react";
 import { FlatList, Modal, Text, TouchableOpacity, View } from "react-native";
 
@@ -7,12 +8,14 @@ const ModalSelect = ({
   setModalVisible,
   handleSelect,
   value,
+  title,
 }: {
-  options: ArrayLike<any>;
+  options: PaymentOptions[];
   modalVisible: boolean;
   setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
   handleSelect: (name: string) => void;
   value: string;
+  title: string;
 }) => {
   const handleClick = (name: string) => {
     handleSelect(name);
@@ -31,13 +34,13 @@ const ModalSelect = ({
         className="flex-1 bg-[#0000004c] justify-center items-center"
       >
         <View className="bg-white rounded-md w-2/3 py-7 px-4">
-          <Text className="text-xl mb-4">Pilih Metode Top Up :</Text>
+          <Text className="text-xl mb-4">{title}</Text>
           <FlatList
             data={options}
             keyExtractor={(item, index) => index.toString()}
             renderItem={({ item }) => (
               <TouchableOpacity
-                onPress={() => handleClick(item.name)}
+                onPress={() => handleClick(item.value)}
                 className="px-2 py-3 border-t border-[#E1E1E1]"
               >
                 <Text className="text-lg">{item.name}</Text>
