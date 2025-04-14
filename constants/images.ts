f436239b5e485a1ebe0f7876f246cafdfbf62e95
kobulwidodo <fadhilhan01@gmail.com>
2025-04-14 15:20:31 +0700

    fix: dark mode

--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,5 @@
 import logo from "@/assets/images/logo.png";
+import moon from "@/assets/images/moon.png";
 import profileBlank from "@/assets/images/profile-blank.png";
 import profile from "@/assets/images/profile.png";
 import sun from "@/assets/images/sun.png";
@@ -7,5 +8,6 @@ export default {
   logo,
   profile,
   sun,
+  moon,
   profileBlank,
 };
